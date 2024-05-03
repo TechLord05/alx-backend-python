41de541c856a66d72942bf4ad13aa9c01aa9c447
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-05-03 02:49:06 +0000

    Return the value linked to a key in a dictionary.

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-""" type-annotated function to_str that takes a float n as
-argument and returns the string representation of the float.
+""" Module that contains type-annotated function to_str that takes a float n
+as an argument and returns the string representation of the float.
 """
 
 
-def to_str(n: str) -> str:
-    """Function that takes a float n as argument and returns
-    the string representation of the float.
+def to_str(n: float) -> str:
+    """Function to return the string representation of a float.
     """
     return str(n)
