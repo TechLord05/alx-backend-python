5b68d6e413547b7c675fcee51779ac26f7665daf
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-05-03 02:33:18 +0000

    Function that takes a list of elements lst as argument     and returns a list of tuples, each containing an element     and its length.

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Module that annotates the below function's parameters
+    and return values with the appropriate types
+
+-   def element_length(lst):
+        return [(i, len(i)) for i in lst]
+"""
+
+from typing import List, Tuple, Iterable, Sequence
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Function that takes a list of elements lst as argument
+    and returns a list of tuples, each containing an element
+    and its length.
+    """
+    return [(i, len(i)) for i in lst]
