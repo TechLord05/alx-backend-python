aeb5b4bc8d495c281f867d572ca43c203f07b077
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-05-03 02:37:54 +0000

    Function that takes a list lst as argument    and returns its first element if it exists, otherwise None.

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Module that annotates the below function's parameters
+    and return values with the appropriate types
+
+-   def safe_first_element(lst):
+    if lst:
+        return lst[0]
+    else:
+        return None
+"""
+
+from typing import List, Union, Any, Sequence
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Function that takes a list lst as argument
+    and returns its first element if it exists, otherwise None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
